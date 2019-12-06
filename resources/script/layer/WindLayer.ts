82b4b2cf584e4a2b051dcbcb4aef121b9a44f337
Fabian Brosda <f.brosda@gmx.de>
2019-12-06 21:56:48 +0100

    play button for time steps

--- resources/script/layer/WindLayer.ts
+++ resources/script/layer/WindLayer.ts
@@ -5,9 +5,11 @@ import { ShaderType } from "../util/util.js";
 export default class WindLayer extends AbstractCustomLayer {
   shaders: Promise<string[]>;
   visibleCheckbox: HTMLInputElement;
+  numParticlesInput: HTMLInputElement;
   resolutionSelect: HTMLSelectElement;
   dateSelect: HTMLSelectElement;
-  numParticlesInput: HTMLInputElement;
+  playButton: HTMLButtonElement;
+  playInterval?: any;
 
   constructor(map?: mapboxgl.Map) {
     super("wind", map);
@@ -24,16 +26,21 @@ export default class WindLayer extends AbstractCustomLayer {
     ) as HTMLInputElement;
     this.createVisibleCheckbox();
 
-    this.resolutionSelect = document.getElementById("resolution") as HTMLSelectElement;
+    this.numParticlesInput = document.getElementById(
+      "numParticles"
+    ) as HTMLInputElement;
+    this.createNumParticlesInput();
+
+    this.resolutionSelect = document.getElementById(
+      "resolution"
+    ) as HTMLSelectElement;
     this.createResolutionSelect();
 
     this.dateSelect = document.getElementById("date") as HTMLSelectElement;
     this.createDateSelect();
 
-    this.numParticlesInput = document.getElementById(
-      "numParticles"
-    ) as HTMLInputElement;
-    this.createNumParticlesInput();
+    this.playButton = document.getElementById("play") as HTMLButtonElement;
+    this.createPlayButton();
   }
 
   async onAdd(map: mapboxgl.Map, gl: WebGLRenderingContext): Promise<void> {
@@ -76,7 +83,9 @@ export default class WindLayer extends AbstractCustomLayer {
     if (this.layer) {
       const layer = this.layer as WindGlLayer;
       const date = this.dateSelect.options[this.dateSelect.selectedIndex];
-      const resolution = this.resolutionSelect.options[this.resolutionSelect.selectedIndex];
+      const resolution = this.resolutionSelect.options[
+        this.resolutionSelect.selectedIndex
+      ];
       // this.toggle();
       await layer.loadWindData(date.value, resolution.value);
       // this.toggle();
@@ -90,10 +99,35 @@ export default class WindLayer extends AbstractCustomLayer {
     }
   }
 
+  private togglePlay(): void {
+    const doPlay = this.playButton.innerHTML === "Play";
+    const func = updateDate.bind(this);
+
+    if (doPlay) {
+      this.playButton.innerHTML = "Stop";
+      this.playInterval = setTimeout(func, 4000);
+    } else {
+      this.playButton.innerHTML = "Play";
+      clearInterval(this.playInterval);
+      delete this.playInterval;
+    }
+
+    async function updateDate(this: WindLayer): Promise<void> {
+      const curIndex = this.dateSelect.selectedIndex;
+      const length = this.dateSelect.options.length;
+      this.dateSelect.selectedIndex = (curIndex + 1) % length;
+      await this.changeDateAndResolution();
+
+      this.playInterval = setTimeout(func, 3000);
+    }
+  }
+
   private createResolutionSelect(): void {
     const f = this.changeDateAndResolution.bind(this);
     this.resolutionSelect.addEventListener("change", f);
-    this.handler.push(() => this.resolutionSelect.removeEventListener("change", f));
+    this.handler.push(() =>
+      this.resolutionSelect.removeEventListener("change", f)
+    );
   }
 
   private createDateSelect(): void {
@@ -108,11 +142,21 @@ export default class WindLayer extends AbstractCustomLayer {
       this.dateSelect.appendChild(child);
     }
 
-    const f = this.changeDateAndResolution.bind(this);
+    const f = (): void => {
+      if (this.playInterval) {
+        this.togglePlay();
+      }
+      this.changeDateAndResolution();
+    };
     this.dateSelect.addEventListener("change", f);
     this.handler.push(() => this.dateSelect.removeEventListener("change", f));
   }
 
+  private createPlayButton(): void {
+    const f = this.togglePlay.bind(this);
+    this.playButton.addEventListener("click", f);
+    this.handler.push(() => this.playButton.removeEventListener("click", f));
+  }
   private createNumParticlesInput(): void {
     const f = this.setNumParticles.bind(this);
     this.numParticlesInput.addEventListener("change", f);
