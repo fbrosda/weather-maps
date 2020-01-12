5ca0b0ef2f7f754dee369672b43522df2bd82821
Fabian Brosda <f.brosda@gmx.de>
2020-01-12 09:47:52 +0100

    missing string pad for month

--- src/datafetcher/NoaaDataFetcher.ts
+++ src/datafetcher/NoaaDataFetcher.ts
@@ -107,7 +107,9 @@ export default abstract class NoaaDataFetcher extends DataFetcher {
 
     function formatDate(date?: Date): string {
       const d = date ?? new Date();
-      return `${d.getFullYear()}${d.getMonth() + 1}${d
+      return `${d.getFullYear()}${(d.getMonth() + 1)
+        .toString()
+        .padStart(2, "0")}${d
         .getDate()
         .toString()
         .padStart(2, "0")}`;
